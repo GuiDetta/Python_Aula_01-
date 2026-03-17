51be739360caad8b70fc8c569a39f89f86b79c3d
GuiDetta <guilherme.detta2@gmail.com>
2026-03-16 22:42:25 -0300

    Aula02-variaveis

---
+++ Aula02-variaveis-operadores/Desafio-aula.py
@@ -0,0 +1,11 @@
+nome = input("Digite seu nome: ")
+print(f'Boas vindas {nome}')
+
+dia = int(input("Digite o dia em que nasceu: "))
+mês = int(input("Digite o mês em que nasceu: "))
+ano = int(input("Digite o ano em que nasceu: "))
+print(f'Então você nasceu {dia}/{mês}/{ano}')
+
+x = int(input("Digite um numero:"))
+y = int(input("Digite um numero:"))
+print(x + y)
---
+++ Aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,31 @@
+print("olá mundo")
+
+print(7+4)
+print("7+4")
+print("7" + "4")# CONCATENA STRINGS
+
+# Comentários de 1 linha no python
+
+'''
+        Comentarios de
+        múltiplas
+        linhas
+'''
+
+#VARIÁVEOS
+
+nome = "Alexanre"
+idade = 26 #int
+peso = 70.2 # float
+
+print(nome, idade, peso)
+
+#IMPUT - SIMULAR UM FORMULÁRIO NO CMD
+nome = input("Digite seu nome: ") # str
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+
+print(nome, idade, peso)
+print(f'Oiii, {nome}')
+
+
