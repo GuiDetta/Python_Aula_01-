ce0b1ed1ba232a728f8e19855c2588cff1dca369
GuiDetta <guilherme.detta2@gmail.com>
2026-03-31 22:44:08 -0300

    desafio

--- Aula03-modulos-operacionais/aula03-desafio.py
+++ Aula03-modulos-operacionais/aula03-desafio.py
@@ -1,5 +1,13 @@
 idade = int(input("Digite sua idade: "))
 if idade < 16:
     print("Você não pode votar")
-elif idade <:
+
+elif idade < 18:
+    print("Seu voto é opcional")
+
+elif idade < 70:
+    print("Seu voto é obrigatorio")
+
+else:
+    print("Seu voto é opcional")
 
